6bacb0ff1ec11041d4ed2b1274cad1ab9e087279
Davide Rizzo <davide@metwit.com>
2010-03-31 11:26:54 +0200

    no unicode chars on console please

--- FoxGame/foxgame/structures.py
+++ FoxGame/foxgame/structures.py
@@ -138,15 +138,15 @@ class Direction(object):
         Print the direction using cool Unicode characters.
         """
         dirs = {
-                self.UP       : '↑',
-                self.DOWN     : '↓',
-                self.RIGHT    : '→',
-                self.LEFT     : '←',
-                self.UPRIGHT  : '↗',
-                self.UPLEFT   : '↖',
-                self.DOWNRIGHT: '↘',
-                self.DOWNLEFT : '↙',
-                self.NULL     : ' '
+                self.UP       : 'N',
+                self.DOWN     : 'S',
+                self.RIGHT    : 'W',
+                self.LEFT     : 'L',
+                self.UPRIGHT  : 'NW',
+                self.UPLEFT   : 'NE',
+                self.DOWNRIGHT: 'SW',
+                self.DOWNLEFT : 'SE',
+                self.NULL     : '-'
         }
         return dirs[self.hor, self.vert]
 
