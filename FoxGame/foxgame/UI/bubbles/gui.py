5e52552fd90d27c641d74ab7ef603dcc0c8c8147
Davide Rizzo <davide@metwit.com>
2010-04-01 14:46:52 +0200

    bubbles in a package

--- FoxGame/foxgame/UI/bubbles/gui.py
+++ FoxGame/foxgame/UI/bubbles/gui.py
@@ -8,35 +8,7 @@ import pygame
 from foxgame.structures import Direction
 from foxgame.controller import Brain
 
-try:
-    from pygame.gfxdraw import aacircle, filled_circle
-    use_gfxdraw = True
-except ImportError:
-    from pygame.draw import circle
-    use_gfxdraw = False
-
-
-def draw_circle_gfx(surf, radius, color, pos_x, pos_y):
-    """
-    Circle drawing primitive (with anti-aliasing).
-    """
-    args = surf, pos_x, pos_y, int(radius), pygame.Color(color)
-    filled_circle(*args)
-    aacircle(*args)
-
-def draw_circle_old(surf, radius, color, pos_x, pos_y):
-    """
-    Circle drawing primitive.
-    """
-    circle(surf, pygame.Color(color), (pos_x, pos_y), int(radius))
-
-# Compatibility with old versions of PyGame
-# Prior to 1.9 there was no gfxdraw
-if use_gfxdraw:
-    draw_circle = draw_circle_gfx
-else:
-    draw_circle = draw_circle_old
-
+from draw import draw_circle
 
 class UserBrain(Brain):
     """
