88e62768c781a50b189c49c562f97fd0279c9ec3
Michele Orrù <mmaker@users.noreply.github.com>
2010-04-03 11:04:19 +0200

    has_key is deprecated and bad

--- FoxGame/foxgame/UI/bubbles/bzdraw.py
+++ FoxGame/foxgame/UI/bubbles/bzdraw.py
@@ -20,16 +20,16 @@ class BZManager(object):
         Returns a drawing context.
         """
         return self.contexts[name]
-    
+
     def new_context(self, name='frame'):
         self.remove_context(name)
         return self.get_context(name)
-    
+
     def remove_context(self, name):
-        if self.contexts.has_key(name):
+        if name in self.contexts:
             del self.contexts[name]
-        
-    
+
+
     def draw_all_under(self):
         for ctx in self.contexts.itervalues():
             ctx.draw_under()
@@ -43,7 +43,7 @@ class BZPainter(object):
     """
     Painter for brainz interface.
     """
-    
+
     def __init__(self, gui):
         self.gui = gui
         self.color = (100, 100, 100)
@@ -53,13 +53,13 @@ class BZPainter(object):
         scale = self.gui.scale
         color = options.get('color', self.color)
         draw_circle(surf, radius(), color, *pos(), scale=scale)
-    
+
     def line(self, points, options):
         surf = self.gui.arena
         scale = self.gui.scale
         color = options.get('color', self.color)
         draw_lines(surf, color, False, (p() for p in points), scale=scale)
-    
+
     def vector(self, pos, vec, options):
         surf = self.gui.arena
         scale = self.gui.scale
@@ -67,14 +67,14 @@ class BZPainter(object):
         start = Vector(*pos())
         end = start + vec()
         draw_line(surf, color, start.x, start.y, end.x, end.y, scale=scale)
-    
+
     def highlight(self, gameobj, options):
         surf = self.gui.arena
         scale = self.gui.scale
         color = options.get('color', self.color)
         gameobj = gameobj()
         draw_circle(surf, game_obj.radius/2, color, gameobj.pos, scale=scale)
-    
+
     @property
     def dir_len(self):
         return self.gui.screen_size[0] / 6
