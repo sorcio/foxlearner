67d9d7a6b2eac1d36632728a7652bebd939297fd
Michele Orrù <mmaker@users.noreply.github.com>
2010-04-04 23:03:21 +0200

    libs/__init__.py added _now_ -.-"

---
+++ FoxGame/foxgame/controllers/libs/tests/test_neuralnetwork.py
@@ -0,0 +1,22 @@
+from unittest import TestCase
+from foxgame.controllers.libs.neuralnetwork import NeuralNetwork
+
+class TestNeuralNetwork(TestCase):
+
+    def setUp(self):
+        self.threshold = 0.1
+
+    def test_XOR(self):
+        pat = (
+            ((0, 0), (0, )),
+            ((0, 1), (1, )),
+            ((1, 0), (1, )),
+            ((1, 1), (0, ))
+            )
+
+        n = NeuralNetwork(2, 2, 1)
+        n.train(pat)
+        for i, o in pat:
+            self.assertTrue(n.put(i)[0] - o[0] < self.threshold)
+
+
