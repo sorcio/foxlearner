24c1c9630bcf1e8a98b2fde69330031e5cb5068f
Davide Rizzo <davide@metwit.com>
2010-04-09 17:59:13 +0200

    [merge]

--- FoxGame/foxgame/controllers/benchmark.py
+++ FoxGame/foxgame/controllers/benchmark.py
@@ -1,5 +1,23 @@
 from foxgame.structures import Direction
 from foxgame.controller import PostFilter
+from sys import stdout
+
+
+def simple_print(dst, data):
+    """
+    Print data 'as it is'.
+    """
+    dst.write('\n'.join(data.values()))
+
+
+def core_print(dst, data):
+    """
+    Print data with a simple formatting.
+    """
+    dst.writeline(pawn_name)
+
+    for key, val in data.iteritems():
+        dst.writeline('\t%s : %s;' % (key, val))
 
 
 class Benchmark (PostFilter):
@@ -8,8 +26,37 @@ class Benchmark (PostFilter):
     """
 
     formatter = simple_print
+    dest = None
+
+    def _parse_data(self):
+        # TODO
+        return dict((key, str(val)) for key, val in self.datas.iteritems())
+
+    def set_up(self):
+        self.file = open(self.dest, 'w') if self.dest else stdout
+        self.datas = {
+                'name'     : self.pawn.__class__,
+                'position' : [],
+                'speed'    : [],
+                'accel'    : [],
+                'enemied'  : []
+        }
+
     def update(self, direction, time):
-        return Direction((direction.hor, -direction.vert))
+        self.datas['position'].append(self.pawn.pos)
+        self.datas['speed'].append(self.pawn.speed)
+        self.datas['accel'].append(self.pawn.acc)
+        if self.pawn.__class__.__name__ == 'Fox':
+            self.datas['enemied'] = self.pawn.distance(self.game.hare)
+        else:
+            self.datas['enemied'] = self.pawn.distance(self.nearest_fox)
+
+        # return the same direction
+        return direction
+
+    def tear_down(self):
+        out = self._parse_data()
+        simple_print(self.file, out)
 
 
-def simple_print(**)
+
