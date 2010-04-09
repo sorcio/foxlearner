513477b9f46dafb1c667007cfc75fedf241e79e7
Michele Orrù <mmaker@users.noreply.github.com>
2010-04-09 17:39:20 +0200

    banchmark.py improved a little

--- FoxGame/foxgame/controllers/benchmark.py
+++ FoxGame/foxgame/controllers/benchmark.py
@@ -3,6 +3,23 @@ from foxgame.controller import PostFilter
 from sys import stdout
 
 
+def simple_print(dst, data):
+    """
+    Print data 'as it is'.
+    """
+    dst.write('\n'.join(data.values()))
+
+
+def core_print(dst, data):
+    """
+    Print data with a simple formatting.
+    """
+    dst.writeline(pawn_name)
+
+    for key, val in data.iteritems():
+        dst.writeline('\t%s : %s;' % (key, val))
+
+
 class Benchmark (PostFilter):
     """
     A simple postfilter which stores and returns datas useful for benchmarking.
@@ -12,38 +29,34 @@ class Benchmark (PostFilter):
     dest = None
 
     def _parse_data(self):
-        return dict()
+        # TODO
+        return dict((key, str(val)) for key, val in self.datas.iteritems())
 
     def set_up(self):
-        self.file = open(self.dest, 'w') if dest else stdout
+        self.file = open(self.dest, 'w') if self.dest else stdout
         self.datas = {
                 'name'     : self.pawn.__class__,
                 'position' : [],
                 'speed'    : [],
                 'accel'    : [],
-                'carrots'  : []
+                'enemied'  : []
         }
 
     def update(self, direction, time):
-        return Direction((direction.hor, -direction.vert))
+        self.datas['position'].append(self.pawn.pos)
+        self.datas['speed'].append(self.pawn.speed)
+        self.datas['accel'].append(self.pawn.acc)
+        if self.pawn.__class__.__name__ == 'Fox':
+            self.datas['enemied'] = self.pawn.distance(self.game.hare)
+        else:
+            self.datas['enemied'] = self.pawn.distance(self.nearest_fox)
+
+        # return the same direction
+        return direction
 
     def tear_down(self):
         out = self._parse_data()
-        simple_print(out)
+        simple_print(self.file, out)
 
 
-def simple_print(dst, **kwargs):
-    """
-    Print kwargs 'as it is'.
-    """
-    dst.write('\n'.join(kwargs.values()))
 
-
-def core_print(dst, **kwargs):
-    """
-    Print kwargs with a simple formatting.
-    """
-    dst.writeline(pawn_name)
-
-    for key, val in kwargs.iteritems():
-        dst.writeline('\t%s : %s;', % (key, str(val)))
