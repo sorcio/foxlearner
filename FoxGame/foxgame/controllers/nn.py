d0eb736d3e035cf6184f21fee35fda61de8b9f7d
Daniele Iamartino <Otacon22@users.noreply.github.com>
2010-04-10 18:30:21 +0200

    Fixed a bug in neuralnetwork lib and nn controller 'working'

---
+++ FoxGame/foxgame/controllers/nn.py
@@ -0,0 +1,124 @@
+"""
+nn.py: Brains which provide a neural network
+       to move foxes/hare.
+
+"""
+
+from foxgame.controller import Brain
+from foxgame.structures import Vector, Direction
+from libs.neuralnetwork import NeuralNetwork
+from foxgame.controllers.processors import SaveData
+import shelve
+from os.path import join, exists
+from os import remove
+
+class FoxBrain(Brain):
+    """
+    A controller which uses a neural network to follow the hare.
+    """
+    # TODO: Not working, rewrite the class looking at the other.
+    _net_struct = 8, 10
+    _net_data = join('foxgame', 'controllers', 'libs', 'synapsis_fox.db')
+
+    def set_up(self):
+        """
+        Used to load neural network data from a file
+        """
+        self.network = NeuralNetwork(*self._net_struct)
+        self.network.load(self._net_data)
+
+    def update(self, time):
+        """
+        The neural network recives in input the following data:
+        Hare position, Fox position, Carrot position and hare speed.
+        """
+
+        data = (self.game.hare.pos.x, self.game.hare.pos.y,
+                self.pawn.pos.x, self.pawn.pos.y,
+                self.game.carrot.pos.x, self.game.carrot.pos.y,
+                self.game.hare.speed.x, self.game.hare.speed.y)
+
+        output = []
+        for value in self.network.put(data):
+            output.append(int(round(value)))
+
+        return Direction(output)
+
+    def tear_down(self):
+        """
+        It saves the neural network weights into a file
+        """
+        self.network.save(self._net_data)
+
+
+class HareBrain(Brain):
+    """
+    A controller which uses a neural network to escape from the fox.
+    """
+    training = True
+    _net_struct = 8, 20
+    _net_data = join('foxgame', 'controllers', 'libs', 'synapsis_hare.db')
+
+    def set_up(self):
+        """
+        Used to load neural network data from a file
+        """
+        
+        # XXX Change this!!!
+        if HareBrain.training:
+            print "Addestramento"
+            if exists(self._net_data):
+                remove(self._net_data)
+            self.train_network()
+            HareBrain.training = False
+
+        self.network = NeuralNetwork(*self._net_struct)
+        self.network.load(self._net_data)
+
+    def update(self, time):
+        """
+        The neural network recives in input the following data:
+        Hare position, Fox position, Carrot position and hare speed.
+        """
+
+        data = (self.game.hare.pos.x, self.game.hare.pos.y,
+                self.game.carrot.pos.x, self.game.carrot.pos.y,
+                self.pawn.pos.x, self.pawn.pos.y,
+                self.game.hare.speed.x, self.game.hare.speed.y)
+                
+        output = [int(round(value)) for value in self.network.put(data)]
+
+
+        return Direction(output)
+
+    def tear_down(self):
+        """
+        It saves the neural network weights into a file
+        """
+        self.network.save(self._net_data)
+
+    def train_network(self):
+        """
+        """
+        
+        logfile = SaveData.logfile
+        pattern = []
+        
+        db = shelve.open(logfile)
+        if db == {}:
+            raise IOError('File %s empty' % logfile)
+            
+        for f_pos, h_pos, c_pos, h_spd, h_dir in zip(db['fox.pos'], db['hare.pos'],
+                                db['carrot.pos'], db['hare.speed'], db['hare.dir']):
+            
+            example = [[f_pos.x/self.game.size.x, f_pos.y/self.game.size.y,
+                        h_pos.x/self.game.size.x, h_pos.y/self.game.size.y,
+                        c_pos.x/self.game.size.x, c_pos.y/self.game.size.y,
+                        h_spd.x/self.game.size.x, h_spd.y/self.game.size.y],
+                       tuple(h_dir)]
+            pattern.append(example)
+
+        n = NeuralNetwork(*HareBrain._net_struct)
+        n.train(pattern, 10)
+        n.save(HareBrain._net_data)
+
---
+++ FoxGame/foxgame/utils.py
@@ -0,0 +1,100 @@
+from collections import deque
+
+class ShortDequeImpl(deque):
+    """
+    Deque with bounded length.
+    
+    Mimics Python 2.6 deque with maxlen.
+    
+    Once a shortdeque is full, when new items are added,
+    a corresponding number of items are discarded from 
+    the opposite end.
+    """
+    
+    def __init__(self, iterable='', maxlen=0):
+        if maxlen:
+            if maxlen < 0:
+                raise ValueError('maxlen must be non-negative')
+            self._maxlen = maxlen
+        else:
+            self._maxlen = 0
+        super(ShortDequeImpl, self).__init__()
+        self.clear()
+        assert len(self) == 0
+        if iterable:
+            self.extend(iterable)
+
+    def __copy__(self):
+        if hasattr(self, '_maxlen'):
+            result = self.__class__(self, self._maxlen)
+        else:
+            result = self.__class__(self)
+
+        return result
+
+    def __deepcopy__(self, memo={}):
+        from copy import deepcopy
+        result = self.__class__()
+        memo[id(self)] = result
+        result.__init__(deepcopy(tuple(self), memo))
+
+        return result
+
+    def __repr__(self):
+        lrepr = ', '.join(repr(x) if x is not self else '[...]' for x in self)
+        if self._maxlen:
+            s = "deque([%s], maxlen=%d)" % (lrepr, self._maxlen)
+        else:
+            s = "deque([%s])" % (lrepr,)
+        return s
+
+    def append(self, x):
+        if self._maxlen and len(self) == self._maxlen:
+            self.popleft()
+        super(ShortDequeImpl, self).append(x)
+    
+    def appendleft(self, x):
+        if self._maxlen and len(self) == self._maxlen:
+            self.pop()
+        super(ShortDequeImpl, self).appendleft(x)
+    
+    def extend(self, iterable):
+        for x in iterable:
+            self.append(x)
+    
+    def extendleft(self, iterable):
+        for x in iterable:
+            self.appendleft(x)
+
+
+try:
+    # Are we on a platform that supports deque with maxlen?
+    # (e.g. CPython >= 2.6)
+    deque(maxlen=1)
+    ShortDeque = deque
+except TypeError:
+    # If not then assume we have CPython 2.5 compatibility
+    # and use our compatibility class.
+    ShortDeque = ShortDequeImpl
+
+from functools import partial
+class ProxyTestSD(object):
+    """
+    Proxy to compare ShortDeque and deque functionality.
+    
+    To be run on 2.6 only.
+    """
+    
+    def __init__(self, *args, **kwargs):
+        self.a = deque(*args, **kwargs)
+        self.b = ShortDequeImpl(*args, **kwargs)
+    
+    def __getattr__(self, name):
+        def proxy(name, *args, **kwargs):
+            r1 = getattr(self.a, name)(*args, **kwargs)
+            r2 = getattr(self.b, name)(*args, **kwargs)
+            assert r1 == r2
+            assert self.a == self.b
+            return r1
+        return partial(proxy, name)
+
