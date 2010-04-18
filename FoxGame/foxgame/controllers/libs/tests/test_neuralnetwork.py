6403c41106707cdab44ab7eae1adf40f29af03e7
Michele Orrù <mmaker@users.noreply.github.com>
2010-04-18 12:29:35 +0200

    test or, and, xand added on neuralnetwork library

--- FoxGame/foxgame/controllers/libs/tests/test_neuralnetwork.py
+++ FoxGame/foxgame/controllers/libs/tests/test_neuralnetwork.py
@@ -9,36 +9,78 @@ class TestNeuralNetwork(TestCase):
     def setUp(self):
         """
         """
-        self.xor_pat = (
-                    ((0, 0), (0, )),
-                    ((0, 1), (1, )),
-                    ((1, 0), (1, )),
-                    ((1, 1), (0, ))
-                    )
         self.threshold = 0.1
-        self.error_threshold = 0.01
+        self.error_threshold = 0.2
 
     def test_XOR(self):
         """
         Test neuralnetwork library with XOR rule.
         """
+        xor_pat = (
+                ((0, 0), (0, )),
+                ((0, 1), (1, )),
+                ((1, 0), (1, )),
+                ((1, 1), (0, ))
+                  )
 
         n = NeuralNetwork(2, 2, 1)
-        n.train(self.xor_pat)
+        n.train(xor_pat)
 
-        for i, o in self.xor_pat:
+        for i, o in xor_pat:
+            self.assertAlmostEqual(n.put(i)[0], o[0], 1)
             self.assertTrue(n.put(i)[0] - o[0] < self.threshold)
+
+    def test_OR(self):
+        """
+        Test neuralnetwork library with OR rule.
+        """
+        or_pat = (
+               ((0, 0), (0, )),
+               ((1, 0), (1, )),
+               ((0, 1), (1, )),
+               ((1, 1), (1, ))
+                 )
+
+        n = NeuralNetwork(2, 15, 1)
+        n.train(or_pat)
+
+        for i, o in or_pat:
+            self.assertAlmostEqual(n.put(i)[0], o[0], 0)
+            self.assertTrue(n.put(i)[0] - o[0] < self.threshold)
+
+    def test_NAND(self):
+        nand_pat = (
+                 ((0, 0), (1, )),
+                 ((1, 0), (0, )),
+                 ((0, 1), (0, )),
+                 ((1, 1), (1, ))
+                   )
+
+        n = NeuralNetwork(2, 8, 1)
+        n.train(nand_pat)
+
+        for i, o in nand_pat:
+            self.assertAlmostEqual(n.put(i)[0], o[0], 0)
+            self.assertTrue(n.put(i)[0] - o[0] < self.threshold)
+
 
     def test_des_error(self):
         """
         Test training with desidered error mode.
         """
+        and_pat = (
+               ((0, 0), (0, )),
+               ((1, 0), (0, )),
+               ((0, 1), (0, )),
+               ((1, 1), (1, ))
+                 )
+
 
         n = NeuralNetwork(2, 2, 1)
         r_value = n._rand(0.003, 0.0001)
-        err, iterations = n.train(self.xor_pat, None, 0.5, r_value)
+        err, iterations = n.train(and_pat, None, 0.5, r_value)
 
-        for i, o in self.xor_pat:
+        for i, o in and_pat:
             self.assertTrue(n.put(i)[0] - o[0] < self.threshold)
 
         # It checks the value using the relative error
