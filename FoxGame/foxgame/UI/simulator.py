57acf6b54af5929a920c35c604f2e1aef11f837f
Michele Orrù <mmaker@users.noreply.github.com>
2010-04-25 20:30:27 +0200

    benchmarking in simulator added

--- FoxGame/foxgame/UI/simulator.py
+++ FoxGame/foxgame/UI/simulator.py
@@ -4,6 +4,8 @@ simulator.py: a masochistic GUI. Used mainly for tests/controller learning
 """
 
 from __future__ import division
+from collections import defaultdict
+from math import sqrt
 
 from foxgame.structures import Direction
 from foxgame.options import FoxgameOption
@@ -12,14 +14,48 @@ from foxgame.controller import Brain
 from logging import getLogger
 log = getLogger(__name__)
 
-def nulljob(self):
+
+def job_null(self):
     """
     Do nothing.
     """
     pass
 
-def benchmark(self):
-    raise NotImplementedError
+postjob_null = job_null
+
+def job_benchmark(self):
+    """
+    Store some useful informations about benchmarking:
+     - carrots eaten
+     - time elapsed
+    """
+
+    self.store['carrots'].append(self.game.hare.carrots)
+    self.store['time'].append(self.game.time_elapsed)
+
+def postjob_benchmark(self):
+    """
+    Print the average / deviation of previously
+    stored values.
+    """
+    average = lambda samples: sum(samples) / len(samples)
+    deviat  = lambda samples, average: sqrt(sum((x - average)**2
+                                            for x in samples)/len(samples))
+
+    print 'Average:'
+    # carrots
+    caverage =  average(self.store['carrots'])
+    print '\tcarrots: %d' % caverage
+    # time
+    taverage = average(self.store['time'])
+    print '\ttime: %d"' % taverage
+
+    print 'Deviation:'
+    # carrots
+    print '\tcarrots: %d' % deviat(self.store['carrots'], caverage)
+    # time
+    print '\ttime: %d"' % deviat(self.store['time'], taverage)
+
 
 class RawBrain(Brain):
     """
@@ -50,44 +86,74 @@ class GUI(object):
     A simple interface which doesn't show any output on the screen.
     """
 
-    job = nulljob
+    jobs = job_benchmark, postjob_benchmark
+
     games = 1
 
     def __init__(self, game_factory):
         """
-        Set up attributes.
+        Set up attributes:
+          facotries => contains the game factory
+          game      => the current playing game
+          size      => game arena size
+          store     => a dictionary used by the 'job' function
+                       in order to save some datas for the 'postjob'
         """
         #  factories
         self.gfact = game_factory
         self.gfact.harefact.brain = self.gfact.harefact.brain or RawBrain
         #  game
         self.game = self.gfact.new_game()
-        #  shortcuts
-        self.size = self.game.size
+        # store
+        self.store = defaultdict(list)
+
+    # XXX: so bad
+    def job(self):
+        return self.jobs[0](self)
+
+    # XXX: so bad
+    def postjob(self):
+        return self.jobs[1](self)
 
     def tick(self, time):
-        self.job()
         return self.game.tick(time)
+
+    def recycle(self):
+        # end the current game
+        self.job()
+        self.game.end()
+
+        # start a new game
+        self.games -= 1
+        self.game = self.gfact.new_game()
+
+    def quitall(self):
+        self.game.end()
+        self.postjob()
 
 
 def main(gfact):
     # setting up the gui
     ui = GUI(gfact)
     try:
-        while ui.games >  0:
-            if ui.tick(1/32) == False:
+        while ui.games > 0:
+            # if the game is ended or seems to fall into an infinite loop
+            if ui.tick(1/32) == False or ui.game.time_elapsed > 5*60:
                 log.info('game #%d ended' % (GUI.games-ui.games+1))
                 # decrease the game counter
-                ui.games -= 1
+                ui.recycle()
     except KeyboardInterrupt:
         log.info('game stopped by the user')
         print 'game interrupted.'
     finally:
-        ui.game.end()
+        ui.quitall()
 
 
 __extraopts__ = [
                  FoxgameOption('games', type='int'),
-                 FoxgameOption('job', choices={'benchmark': benchmark,
-                                               'none'     : nulljob})
+                 FoxgameOption('jobs', choices={'benchmark': (job_benchmark,
+                                                              postjob_benchmark),
+                                               'none'      : (job_null,
+                                                              postjob_null)
+                                               })
                 ]
