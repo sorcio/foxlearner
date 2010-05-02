2bd19be5d962bfea4acac76b8fc934df5e6da2ac
Michele Orrù <mmaker@users.noreply.github.com>
2010-05-02 09:24:32 +0200

    example controller revisited

--- FoxGame/foxgame/controllers/examples.py
+++ FoxGame/foxgame/controllers/examples.py
@@ -1,7 +1,20 @@
 # -*- coding: utf-8 -*-
 """
-./controllers/example.py: an example of controllers.
+./controllers/example.py: an example of controller.
 """
+
+# import libraries useful for our Brains
+from random import choice as randomchoice
+
+# import basics foxgame modules
+from foxgame.controller import Brain, PostFilter
+from foxgame.structures import Direction
+from foxgame.options import FoxgameOption, task
+
+# set up logger
+from logging import getLogger
+log = getLogger(__name__)
+
 
 # some informations about the author
 __author__ = 'Author\'s Name'
@@ -9,77 +22,53 @@ __mail__ = 'author AT mail DOT com'
 __date__ = '00/00/00'
 
 
-# import basics foxgame modules
-from foxgame.controller import Brain, PostFilter
-from foxgame.structures import Direction
-from foxgame.options import FoxgameOption, task
+# brain classes:
 
-# import libraries useful for our Brains
-from random import choice as randomchoice
-
-# set up logger
-from logging import getLogger
-log = getLogger(__name__)
-
-
-# brain classes
-
+# FoxBrain is the default name for foxes brains
 class FoxBrain(Brain):
     """
     Description of our Brain.
     """
 
-    times = 20
+    def update(self, time):
+        return self.navigate(self.game.hare.pos)
+
+
+# HareBrain is the default name for foxes brains
+class HareBrain(Brain):
+    """
+    Description of our Brain.
+    """
+
+    dir = Direction(Direction.NULL)
 
     def set_up(self):
         """
         Things to do just when the controller is initialized.
         """
-        self.counter = 0
-        self.choice = Direction(Direction.NULL)
-
+        # write something on the logger
         log.debug('Brain created!')
-        log.debug('changing pos each %d times' % self.times)
 
     def update(self, time):
-        # update counter
-        self.counter += 1
-
-        # change direction each 'self.times' times
-        if self.counter % self.times == 0:
-            self.choice = Direction((randomchoice(range(-1, +2)),
-                                     randomchoice(range(-1, +2))))
-            # display in a log message the new direction
-            log.info('New Direction: %s' % self.choice)
-
-        # return a Direction object
-        return self.choice
+        """
+        Return the direction selected with extraoption "dir".
+        """
+        return self.dir
 
     def tear_down(self):
         """
         Things to do just when the controller is destroyed.
         """
-        log.info('Brain done %d moves' % self.counter)
-
-        self.counter = 0
-        log.debug('Brain controller destroyed!')
+        # write something on the logger
+        log.debug('Brain destroyed, direction used: %s' % str(self.dir))
 
     @task
     def task_useless():
         """
         A trivial example of task.
         """
+        # write something on the logger
         log.debug('A task was here')
-        log.debug('Times = %d' % FoxBrain.times)
-
-
-class HareBrain(Brain):
-    """
-    Description of our Brain.
-    """
-
-    def set_up(self):
-        raise NotImplementedError
 
 
 class PFilterExample(PostFilter):
@@ -95,7 +84,4 @@ class PFilterExample(PostFilter):
 
 
 # export options
-__extraopts__ = [FoxgameOption('times', type='int')]
-
-
-
+__extraopts__ = [FoxgameOption('dir', type='direction')]
